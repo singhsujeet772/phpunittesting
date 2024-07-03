7a9cd1316524d493084d55db52b26375a5d12c38
Sujeet <singhsujeet772@gmail.com>
2024-07-03 20:05:31 +0530

    using composer autoload to to require in test classes

--- tests/UserTest.php
+++ tests/UserTest.php
@@ -5,9 +5,7 @@ use PHPUnit\Framework\TestCase;
 class UserTest extends TestCase
 {
     public function testReturnsFullName()
-    {
-        require 'User.php';
-        
+    { 
         $user = new User;                
 
         $user->first_name = "Teresa";
