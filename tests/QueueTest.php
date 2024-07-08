d91610dd2a3bead03ae490ed225524d5f7f29597
Sujeet <singhsujeet772@gmail.com>
2024-07-08 10:36:56 +0530

    fixtures and setup and teardown to remove dependencies of test on another tests

--- tests/QueueTest.php
+++ tests/QueueTest.php
@@ -4,40 +4,38 @@ use PHPUnit\Framework\TestCase;
 
 class QueueTest extends TestCase
 {
+    protected $queue;
+
+    protected function setUp(): void
+    {
+        $this->queue = new Queue;
+    }
+    
+    protected function tearDown(): void
+    {
+        unset($this->queue);
+    }
+
     public function testNewQueueIsEmpty()
     {
-        $queue = new Queue;
-        
-        $this->assertEquals(0, $queue->getCount());    
-        
-        return $queue;
-    }
-    
-    /**
-     * @depends testNewQueueIsEmpty
-    */
-
-    public function testAnItemIsAddedToTheQueue(Queue $queue)
-    {
-        
-        $queue->push('green');
-        
-        $this->assertEquals(1, $queue->getCount());  
-        
-        return $queue;
+        $this->assertEquals(0, $this->queue->getCount());
     }
 
-    /**
-     * @depends testNewQueueIsEmpty
-    */
-    
-    public function testAnItemIsRemovedFromTheQueue(Queue $queue)
+    public function testAnItemIsAddedToTheQueue()
     {
+        $this->queue->push('green');
         
-        $item = $queue->pop();
+        $this->assertEquals(1, $this->queue->getCount());
+    }
+
+    public function testAnItemIsRemovedFromTheQueue()
+    {
+        $this->queue->push('green');
+                
+        $item = $this->queue->pop();
         
-        $this->assertEquals(0, $queue->getCount());
+        $this->assertEquals(0, $this->queue->getCount());
         
-        $this->assertEquals('green', $item);                                        
+        $this->assertEquals('green', $item);
     }    
 }
